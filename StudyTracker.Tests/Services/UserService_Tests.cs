dc24042541109d26133bb3140db3c47508435622
mwerekania <james@mugambi.net>
2024-02-16 03:00:14 +0300

    Add user methodx

---
+++ StudyTracker.Tests/Services/UserService_Tests.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyTracker.Data;
+using StudyTracker.Models;
+using StudyTracker.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyTracker.Tests
+{
+    [TestClass()]
+    public class UserService_Tests
+    {
+        private readonly StudyTrackerDbContext _dbContext;
+        private readonly StudyTrackerDbContext _dbContextFake;
+
+        public UserService_Tests()
+        {
+            var options = new DbContextOptionsBuilder<StudyTrackerDbContext>()
+              .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=StudyTracker;Trusted_Connection=True;MultipleActiveResultSets=true")
+              .Options;
+
+            var fakeOptions = new DbContextOptionsBuilder<StudyTrackerDbContext>()
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=StudyTrackerTest;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
+
+            // Initialize  DbContext with a test database connection
+            _dbContextFake = new StudyTrackerDbContext(fakeOptions); // Provide connection string or options if necessary
+            _dbContext = new StudyTrackerDbContext(options);
+        }
+
+        /* -------------------------------------------------------------------------------------
+         *  Add User Tests
+         *  ------------------------------------------------------------------------------------*/
+
+        // Test AddUser method for Error Message when an exception is thrown
+        [TestMethod()]
+        public void AddUser_Test_Exception()
+        {
+            // Arrange
+            var service = new UserService(_dbContextFake);
+            var user = new User
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = ""
+            };
+
+            string errorMessage;
+            service.AddUser(user, out errorMessage);
+
+            // Act
+            var addedUser = service.AddUser(user, out errorMessage);
+
+            // Assert
+            Assert.IsNotNull(errorMessage);
+            Assert.IsNull(addedUser);
+        }
+
+        // Test AddUser method for successful addition of a user
+        [TestMethod()]
+        public void AddUser_Test_Success()
+        {
+            // Arrange
+            var service = new UserService(_dbContext);
+            var user = new User
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john@doe.me"
+            };
+
+            string errorMessage = "";
+
+            // Act
+            var addedUser = service.AddUser(user, out errorMessage);
+
+            // Assert
+            Assert.IsNotNull(addedUser);
+            Assert.AreEqual(user.FirstName, addedUser.FirstName);
+            Assert.AreEqual(user.LastName, addedUser.LastName);
+            Assert.AreEqual(user.Email, addedUser.Email);
+        }
+
+        /* -------------------------------------------------------------------------------------
+         *   Get User Tests
+         *  ------------------------------------------------------------------------------------*/
+
+        // Test GetUserById method for a user that exists
+        [TestMethod()]
+        public void GetUserById_Test_UserExists()
+        {
+            // Arrange
+            var service = new UserService(_dbContext);
+            var userId = 2;
+
+            // Act
+            var user = service.GetUserById(userId, out string errorMessage);
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual(userId, user.UserId);
+        }
+
+        // Test GetUserById method for a user that does not exist
+        [TestMethod()]
+        public void GetUserById_Test_UserDoesNotExist()
+        {
+            // Arrange
+            var service = new UserService(_dbContext);
+            var userId = 100;
+
+
+            // Act
+            var user = service.GetUserById(userId, out string errorMessage);
+
+            // Assert
+            Assert.IsNull(user);
+            Assert.AreEqual("User not found!", errorMessage);
+        }
+
+        // Test GetUserById method for exception handling
+        [TestMethod()]
+        public void GetUserById_Test_Exception()
+        {
+            // Arrange
+            var service = new UserService(_dbContextFake);
+            var userId = 2;
+
+            // Act
+            var user = service.GetUserById(userId, out string errorMessage);
+
+            // Assert
+            Assert.IsNull(user);
+        }
+
+        // Test GetUserByEmailAndPassword method for a user that exists
+        [TestMethod()]
+        public void GetUserByEmailAndPassword_Test_UserExists()
+        {
+            // Arrange
+            var service = new UserService(_dbContext);
+            var email = "james@mugambi.net";
+            var password = "12345";
+
+            // Act
+            var user = service.GetUserByEmailAndPassword(email, password, out string errorMessage, out bool isSuccess);
+
+            // Assert
+            Assert.IsNotNull(user);
+            Assert.AreEqual(email, user.Email);
+            Assert.AreEqual(password, user.PasswordHash);
+            Assert.IsTrue(isSuccess);
+        }
+
+        // Test GetUserByEmailAndPassword method for a user that does not exist
+        [TestMethod()]
+        public void GetUserByEmailAndPassword_Test_UserDoesNotExist()
+        {
+            // Arrange
+            var service = new UserService(_dbContext);
+            var email = "john@doe.me";
+            var password = "12345";
+
+            // Act
+            var user = service.GetUserByEmailAndPassword(email, password, out string errorMessage, out bool isSuccess);
+
+            // Assert
+            Assert.IsNull(user);
+            Assert.AreEqual("User not found!", errorMessage);
+            Assert.IsFalse(isSuccess);
+        }
+
+        // Test GetUserByEmailAndPassword method for exception handling
+        [TestMethod()]
+        public void GetUserByEmailAndPassword_Test_Exception()
+        {
+            // Arrange
+            var service = new UserService(_dbContextFake);
+            var email = "";
+            var password = "";
+
+            // Act
+            var user = service.GetUserByEmailAndPassword(email, password, out string errorMessage, out bool isSuccess);
+
+            // Assert
+            Assert.IsNull(user);
+            Assert.IsNotNull(errorMessage);
+            Assert.IsFalse(isSuccess);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ StudyTracker/Services/UserService.cs
@@ -0,0 +1,232 @@
+﻿using StudyTracker.Data;
+using StudyTracker.Models;
+
+namespace StudyTracker.Services
+{
+    public class UserService
+    {
+        private readonly StudyTrackerDbContext _dbcontext;
+
+        public UserService(StudyTrackerDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+
+        public User? AddUser(User user, out string errorMessage)
+        {
+            try
+            {
+                // Check if email already exists
+                var userWithEmail = _dbcontext.Users.FirstOrDefault(u => u.Email == user.Email);
+                if (userWithEmail != null)
+                {
+                    errorMessage = "Email already exists!";
+                    return null;
+                }
+                // Check if username already exists
+                var userWithUserName = _dbcontext.Users.FirstOrDefault(u => u.UserName == user.UserName);
+                if (userWithUserName != null)
+                {
+                    errorMessage = "Username already exists!";
+                    return null;
+                }
+                _dbcontext.Users.Add(user);
+                _dbcontext.SaveChanges();
+                errorMessage = "";
+                return user;
+            }
+            catch (System.Exception ex)
+            {
+                errorMessage = ex.Message;
+                return null;
+            }
+        }
+
+        public User? GetUserById(int userId, out string errorMessage)
+        {
+            try
+            {
+                var user  = _dbcontext.Users.FirstOrDefault(u => u.UserId == userId);
+                if (user == null)
+                {
+                    errorMessage = "User not found!";
+                    return null;
+                }
+                else
+                {
+                    errorMessage = "";
+                    return user;
+                }
+                
+            }
+            catch (System.Exception ex)
+            {
+                errorMessage = ex.Message;
+                return null;
+            }
+        }
+
+        // Get user by email and password
+        public User? GetUserByEmailAndPassword(string email, string password, out string errorMessage, out bool isSuccess)
+        {
+            try
+            {
+                var user = _dbcontext.Users.FirstOrDefault(u => u.Email == email && u.PasswordHash == password);
+                if (user == null)
+                {
+                    errorMessage = "User not found!";
+                    isSuccess = false;
+                    return null;
+                }
+                else
+                {
+                    errorMessage = "";
+                    isSuccess = true;
+                    return user;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                errorMessage = ex.Message;
+                isSuccess = false;
+                return null;
+            }
+        }
+
+        // Update user
+        public User? UpdateUser(User user, out string errorMessage)
+        { 
+            try
+            {
+                // check if user exists
+                var userOnFile = _dbcontext.Users.FirstOrDefault(u => u.UserId == user.UserId);
+                if (userOnFile == null)
+                {
+                    errorMessage = "User not found!";
+                    return null;
+                }
+                else
+                {
+                    // Check if user is changing email
+                    if (userOnFile.Email != user.Email)
+                    {
+                        var userWithEmail = _dbcontext.Users.FirstOrDefault(u => u.Email == user.Email);
+                        if (userWithEmail != null)
+                        {
+                            errorMessage = "Email already exists!";
+                            return null;
+                        }
+                    }
+
+                    // Check if user is changing username
+                    if (userOnFile.UserName != user.UserName)
+                    {
+                        var userWithUserName = _dbcontext.Users.FirstOrDefault(u => u.UserName == user.UserName);
+                        if (userWithUserName != null)
+                        {
+                            errorMessage = "Username already exists!";
+                            return null;
+                        }
+                    }
+
+                    // only update particular fields
+                    userOnFile.FirstName = user.FirstName;
+                    userOnFile.LastName = user.LastName;
+                    userOnFile.Email = user.Email;
+                    userOnFile.UserName = user.UserName;
+                    userOnFile.DateModified = System.DateTime.Now;
+
+                    _dbcontext.Users.Update(userOnFile);
+                    _dbcontext.SaveChanges();
+                    errorMessage = "";
+                    return user;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                errorMessage = ex.Message;
+                return null;
+            }
+        }
+
+        // Verify user
+        public User? VerifyUser(int userId, out string errorMessage)
+        {
+            try
+            {
+                var user = _dbcontext.Users.FirstOrDefault(u => u.UserId == userId);
+                if (user == null)
+                {
+                    errorMessage = "User not found!";
+                    return null;
+                }
+                else
+                {
+                    user.IsVerified = true;
+                    _dbcontext.Users.Update(user);
+                    _dbcontext.SaveChanges();
+                    errorMessage = "";
+                    return user;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                errorMessage = ex.Message;
+                return null;
+            }
+        }
+
+        // Change password
+        public User? ChangePassword(int userId, string newPassword, out string errorMessage)
+        {
+            try
+            {
+                var user = _dbcontext.Users.FirstOrDefault(u => u.UserId == userId);
+                if (user == null)
+                {
+                    errorMessage = "User not found!";
+                    return null;
+                }
+                else
+                {
+                    user.PasswordHash = newPassword;
+                    _dbcontext.Users.Update(user);
+                    _dbcontext.SaveChanges();
+                    errorMessage = "";
+                    return user;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                errorMessage = ex.Message;
+                return null;
+            }
+        }
+
+        // Delete user
+        public bool DeleteUser(int userId, out string errorMessage)
+        {
+            try
+            {
+                var user = _dbcontext.Users.FirstOrDefault(u => u.UserId == userId);
+                if (user == null)
+                {
+                    errorMessage = "User not found!";
+                    return false;
+                }
+                else
+                {
+                    user.DateDeleted = DateTime.Now;
+                    _dbcontext.SaveChanges();
+                    errorMessage = "";
+                    return true;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                errorMessage = ex.Message;
+                return false;
+            }
+        }
+    }
+}
